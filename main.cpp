5380d09f254540b88488394d0c59c3db3c09c4a7
Ollie Ford <me@ojford.com>
2015-02-25 23:02:10 +0000

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CARM-Compiler
+//
+//  Created by Ollie Ford on 25/02/2015.
+//  Copyright (c) 2015 OJFord. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
