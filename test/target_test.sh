e5403ec15e0e3338b19b784114c32ddef74fba09
Ollie Ford <me@ojford.com>
2015-03-20 06:44:59 +0000

    implements -flags witharguments

--- test/target_test.sh
+++ test/target_test.sh
@@ -4,6 +4,7 @@
 # 3. scp got/*.output and got/*.log back from the ARM
 #then you might have a nice automated test framework
 
+#One observes, one does
 scp -i ../.arm_key -P 55555 -r run_all.sh ojf13@155.198.117.236:~/test/
 scp -i ../.arm_key -P 55555 -r got/*.s ojf13@155.198.117.236:~/test/got/
 ssh -i ../.arm_key -p 55555 ojf13@155.198.117.236 << ENDCMDS
