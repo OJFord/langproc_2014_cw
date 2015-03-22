9b8616729719f9645a01fda355ed58dc4dc66e4b
Ollie Ford <me@ojford.com>
2015-03-22 16:34:11 +0000

    implements all SyntaxTree derived types (productions still to be done). Moves productions from parser.cpp to productions.cpp

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,6 +6,9 @@
 //  Copyright (c) 2015 OJFord. All rights reserved.
 //
 
+#ifndef __CARM_Compiler__parser__
+#define __CARM_Compiler__parser__
+
 #include "parser/tokens.h"
 #include "parser/syntax-tree.h"
 #include "lexer/lexer.h"
@@ -28,23 +31,92 @@ protected:
 private:
 	TranslationUnit* translation_unit(void);
 	ExternalDeclaration* external_declaration(void);
-	//void function_definition(SyntaxTree*, lexeme);
+	
+	FunctionDefinition* function_definition(void);
 	Declaration* declaration(void);
-	DeclarationSpecifiers* declaration_specifiers(void);
-	//void storage_class_specifier(SyntaxTree*, lexeme);
-	TypeSpecifier* type_specifier(void);
-	//void type_qualifier(SyntaxTree*, lexeme);
-	InitialiserDeclaratorList* initialiser_declarator_list(void);
-	InitialiserDeclarator* initialiser_declarator(void);
-	//Initialiser* initialiser(void);
-	Declarator* declarator(void);
-	DirectDeclarator* direct_declarator(void);
+	DeclarationList* declaration_list(void);
+	
+	JumpStatement* jump_statement(void);
+	IterationStatement* iteration_statement(void);
+	SelectionStatement* selections_tatement(void);
+	ExpressionStatement* expression_statement(void);
+	CompoundStatement* compound_statement(void);
+	LabeledStatement* labeled_statement(void);
+	Statement* statement(void);
+	StatementList* statement_list(void);
+
+	Initialiser* initialiser(void);
+	InitialiserList* initialiser_list(void);
+
+	TypeDefName* typedef_name(void);
+	DirectAbstractDeclarator* direct_abstract_declarator(void);
+	AbstractDeclarator* abstract_declarator(void);
+
+	TypeName* type_name(void);
 	Identifier* identifier(void);
-	//ConstantExpression* constant_expression(void);
-	//ParamaterTypeList* paramater_type_list(void);
-	//IdentifierList* identifier_list(void);
+	IdentifierList* identifier_list(void);	
+
+	ParameterDeclaration* parameter_declaration(void);
+	ParameterList* parameter_list(void);
+	ParameterTypeList* paramater_type_list(void);
+	
+	TypeQualifier* type_qualifier(void);
+	TypeQualifierList* type_qualifier_list(void);
+
+	Pointer* pointer(void);
+	DirectDeclarator* direct_declarator(void);
+	Declarator* declarator(void);
+
+	Enumerator* enumerator(void);
+	EnumeratorList* enumeratorlist(void);
+	EnumerationSpecifier* enumeration_specifier(void);
+
+	StructDeclarator* struct_declarator(void);
+	StructDeclaratorList* struct_declarator_list(void);
+	SpecifierQualifierList* specifier_qualifier_list(void);
+	StructDeclaration* struct_declaration(void);
+	StructDeclarationList* struct_declaration_list(void);
+	StructOrUnion* struct_or_union(void);
+	StructOrUnionSpecifier* struct_or_union_specifier(void);
+
+	TypeSpecifier* type_specifier(void);
+	StorageClassSpecifier* storage_class_specifier(void);
+	InitialiserDeclarator* initialiser_declarator(void);
+	InitialiserDeclaratorList* initialiser_declarator_list(void);
+	DeclarationSpecifiers* declaration_specifiers(void);
+	
+	Expression* expression(void);
+	AssignmentExpression* assignment_expression(void);
+	ConstantExpression* constant_expression(void);
+	ConditionalExpression* conditional_expression(void);
+	
+	LogicalORExpression* logical_or_expression(void);
+	LogicalANDExpression* logical_and_expression(void);
+	InclusiveORExpression* inclusive_or_expression(void);
+	ExclusiveORExpression* exclusive_or_expression(void);
+	ANDExpression* and_expression(void);
+	EqualityExpression* equality_expression(void);
+	RelationalExpression* relational_expression(void);
+	
+	ShiftExpression* shift_expression(void);
+	AdditiveExpression* additive_expression(void);
+	MultiplicativeExpression* multiplicative_expression(void);
+	
+	CastExpression* cast_expression(void);
+	UnaryExpression* unary_expression(void);
+	ArgumentExpressionList* argument_expression_list(void);
+	PostfixExpression* postfix_expression(void);
+	PrimaryExpression* primary_expression(void);
+
+	StringLiteral* string_literal(void);
+	CharacterConstant* character_constant(void);
+	EnumerationConstant* enumeration_constant(void);
+	IntegerConstant* integer_constant(void);
+	FloatingConstant* floating_constant(void);
+	Constant* constant(void);
 
 	Lexer* lexer;
 	SymbolTable& symtbl;
 };
 
+#endif /* defined(__CARM_Compiler__parser__) */
